bccffbbc7ab5998584cb0701fa5d286130440474
Sanjyoth <sanjyoth.satish@gmail.com>
2018-04-17 12:36:21 +0530

    Remove duplicated directory

--- wp-content/plugins/oasis-workflow-pro/js/pages/subpages/restrict-edit.js
+++
@@ -1,8 +0,0 @@
-jQuery( document ).ready( function () {
-
-   // for non-assignees in a given workflow task, we should hide the "save action",
-   // so that the non-assignee is not able to edit the post outside the workflow.
-   jQuery('#save-action').hide();
-
-} );
-
